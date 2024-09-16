8926920854aa425e520ea8296d5935255a60a304
Alex <alex11ullmann@gmail.com>
2024-09-16 10:24:08 -0300

    corregi js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch ("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
     let i=1;
     data.results.forEach(pokemon =>{
         api.innerHTML += `<div><h4>Nombre = ${pokemon.name} </h4>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${i}.png" alt="imagen de ${pokemon.name}"</div>`
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${i}.png" alt="imagen de ${pokemon.name}"></div>`
         i++;
 })})
-.catch(err => alert ("Ocurrio un error"))
+.catch(err => console.log ("Ocurrio un error", err))
